f98062e99eb0b45b6a5ad893442064998eb2983f
Borris <manaloborris153@gmail.com>
2026-04-09 01:30:24 +0800

    Fix Azure MySQL username format

--- config.php
+++ config.php
@@ -4,15 +4,15 @@
  */
 
 //Dev or Prod
-define('IS_DEV', false);
+define('IS_DEV', true);
 
 //Database Config
 define('DB_DRIVER', 'mysql');
 define('DB_HOST', 'actcrud-db-borris-2026.mysql.database.azure.com');
 define('DB_PORT', '3306');
 define('DB_NAME', 'manalo_borris');
-define('DB_USERNAME', 'actcrudadmin');
-define('DB_PASSWORD', 'REPLACE_WITH_YOUR_AZURE_DB_PASSWORD');
+define('DB_USERNAME', 'actcrudadmin@actcrud-db-borris-2026');
+define('DB_PASSWORD', 'Actcrud!2026Db');
 define('DB_CHARSET', 'utf8mb4');
 define('DB_PREFIX', '');
 define('DB_PATH', '');
