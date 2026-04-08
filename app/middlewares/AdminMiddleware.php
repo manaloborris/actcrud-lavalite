48a36834abb6792d304f790ca7eac84ab21818d9
Borris <manaloborris153@gmail.com>
2026-04-09 01:58:18 +0800

    Redirect non-admin users to view page on admin routes

--- app/middlewares/AdminMiddleware.php
+++ app/middlewares/AdminMiddleware.php
@@ -1,9 +1,7 @@
 <?php
 return function ($requestMethod, $params) {
     if (empty($_SESSION['role']) || $_SESSION['role'] !== 'admin') {
-        http_response_code(403);
-        $error = 'Admin access required.';
-        include dirname(__DIR__) . '/views/errors/403.php';
+        header('Location: ' . url('crud/view'));
         exit;
     }
 
